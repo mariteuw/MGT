c2e312450c20a05c56521cfb6889026839e07acb
dioppy <dioppy@dioppy-laptop.(none)>
2013-05-18 23:22:10 +0000

    mgt commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Mgt::Application.config.secret_token = '10b47f6d07c0048569ad73ca15daf4018f8ec6f5b703005816313990359c437801bc3494b92b43e6d4ddd5f03d2962b64e985ebce8f91854d8757b57b99e08de'
